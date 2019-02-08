456616d28019fe139e37469e220e2586e358a957
schan40 <45015781+schan40@users.noreply.github.com>
2019-02-08 16:01:01 -0500

    Add files via upload
    Live Plotting for GUI

---
+++ Live Graph.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as pyplot
+import matplotlib.animation as animation
+
+fig = pyplot.figure()
+ax1 = fig.add_subplot(111)
+
+def refreshGraphData(i):
+    print("Refreshing data...")
+    graphData = open("input_data.txt", "r").read()
+    lines = graphData.split("\n")
+    xValues = []
+    yValues = []
+    for line in lines:
+        if len(line) > 1:
+            x, y = line.split(",")
+            xValues.append(x)
+            yValues.append(y)
+    ax1.clear()
+    ax1.plot(xValues, yValues)
+ani = animation.FuncAnimation(fig, refreshGraphData, interval=1000)
+pyplot.show()
