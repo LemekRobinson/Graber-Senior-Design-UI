2560e98df7b82406b7bcbcf9cfce3e2ba5d3c6ba
Lemek Robinson <lemekr@gmail.com>
2019-04-18 17:37:27 -0400

    update kivy code

--- UImain.py
+++ UImain.py
@@ -11,134 +11,28 @@ from kivy.core.window import Window
 import matplotlib
 matplotlib.use('module://kivy.garden.matplotlib.backend_kivy')
 from kivy.garden.matplotlib.backend_kivyagg import FigureCanvasKivyAgg, NavigationToolbar2Kivy, FigureCanvas
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt; plt.rcdefaults()
 from kivy.uix.popup import Popup
-from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
+from kivy.uix.screenmanager import ScreenManager, Screen, SwapTransition
 import socket
 import pickle
 import struct  ## new
 import cv2
-from Methods import Sensors
-from kivy.graphics import Color, Line, Rectangle
+from Methods import Sensors,TheGuage
 import numpy as np
-from matplotlib.patches import Circle, Wedge, Rectangle
-from matplotlib import cm
 import random
 import datetime
 from matplotlib.dates import DateFormatter
 import pandas as pd
 import matplotlib.style as style
+import mqttDemoScripts.guiSubscriber as guiSub
+import mqttDemoScripts.controlPublisher as controlPub
 
 style.use('seaborn-poster')
 style.use('ggplot')
 matplotlib.rcParams['font.family'] = "serif"
 
 Builder.load_file("app.kv")
-
-class TheGuage():
-    def __init__(self):
-        pass
-
-    def degree_range(self,n):
-        start = np.linspace(0, 180, n+1, endpoint=True)[0:-1]
-        end = np.linspace(0, 180, n+1, endpoint=True)[1::]
-        mid_points = start + ((end-start)/2.)
-        return np.c_[start, end], mid_points
-
-    def rot_text(self,ang):
-        rotation = np.degrees(np.radians(ang) * np.pi / np.pi - np.radians(90))
-        return rotation
-
-
-    def gauge(self,labels=['LOW', 'MEDIUM', 'HIGH', 'VERY HIGH', 'EXTREME'], colors='jet_r', arrow=1, title='', fname=False):
-        """
-        some sanity checks first
-        """
-
-        N = len(labels)
-        if arrow > N:
-            raise Exception("\n\nThe category ({}) is greated than \
-            the length\nof the labels ({})".format(arrow, N))
-
-        """
-        if colors is a string, we assume it's a matplotlib colormap
-        and we discretize in N discrete colors 
-        """
-
-        if isinstance(colors, str):
-            cmap = cm.get_cmap(colors, N)
-            cmap = cmap(np.arange(N))
-            colors = cmap[::-1, :].tolist()
-        if isinstance(colors, list):
-            if len(colors) == N:
-                colors = colors[::-1]
-            else:
-                raise Exception("\n\nnumber of colors {} not equal \
-                to number of categories{}\n".format(len(colors), N))
-
-        """
-        begins the plotting
-        """
-
-        fig3, ax3 = plt.subplots()
-        canvas3 = fig3.canvas
-        ang_range, mid_points = self.degree_range(N)
-        print(ang_range)
-
-        labels = labels[::-1]
-
-        """
-        plots the sectors and the arcs
-        """
-        patches = []
-        for ang, c in zip(ang_range, colors):
-            # sectors
-            patches.append(Wedge((0., 0.), .4, *ang, facecolor='w', lw=2))
-            # arcs
-            patches.append(Wedge((0., 0.), .4, *ang, width=0.10, facecolor=c, lw=2, alpha=0.5))
-
-        [ax3.add_patch(p) for p in patches]
-
-        """
-        set the labels (e.g. 'LOW','MEDIUM',...)
-        """
-
-        for mid, lab in zip(mid_points, labels):
-            ax3.text(0.35 * np.cos(np.radians(mid)), 0.35 * np.sin(np.radians(mid)), lab, horizontalalignment='center', verticalalignment='center', fontsize=14, fontweight='bold', rotation=self.rot_text(mid))
-
-        """
-        set the bottom banner and the title
-        """
-        r = Rectangle((-0.4, -0.1), 0.8, 0.1, facecolor='w', lw=2)
-        ax3.add_patch(r)
-
-        ax3.text(0, -0.05, title, horizontalalignment='center', verticalalignment='center', fontsize=22, fontweight='bold')
-
-        """
-        plots the arrow now
-        """
-
-        pos = mid_points[abs(arrow - N)]
-
-        ax3.arrow(0, 0, 0.225 * np.cos(np.radians(pos)), 0.225 * np.sin(np.radians(pos)), width=0.04, head_width=0.09, head_length=0.1, fc='k', ec='k')
-
-        ax3.add_patch(Circle((0, 0), radius=0.02, facecolor='k'))
-        ax3.add_patch(Circle((0, 0), radius=0.01, facecolor='w', zorder=11))
-
-        """
-        removes frame and ticks, and makes axis equal and tight
-        """
-
-        ax3.set_frame_on(False)
-        ax3.axes.set_xticks([])
-        ax3.axes.set_yticks([])
-        ax3.axis('equal')
-        plt.tight_layout()
-
-        if fname:
-            fig3.savefig(fname, dpi=200)
-
-        return canvas3
 
 class KivyCamera(Image):
 
@@ -157,7 +51,7 @@ class KivyCamera(Image):
         self.s.bind((HOST, PORT))
         print('Socket bind complete')
         try:
-            self.s.listen(2)
+            self.s.listen(1)
             print('Socket now listening')
             self.conn, self.addr = self.s.accept()
             self.data = b""
@@ -204,26 +98,154 @@ class LoginScreen(Screen):
     def __init__(self, **kwargs):
         super(LoginScreen, self).__init__(**kwargs)
         self.loginFail = Popup(title='Login Fail', content=Label(text='user: username\npass: password'), size_hint=(.4,.4),auto_dismiss=True)
+        self.initController()
+        Clock.schedule_interval(self.Xboxcontroller,1/5)
 
     def verify_credentials(self):
         if self.ids["login"].text == "username" and self.ids["passw"].text == "password":
-            self.manager.current = "main"
+            self.manager.switch_to(MainScreen(name='main'))
             Window.size = (900,600)
         else:
             self.loginFail.open()
-    Window.fullscreen = False
-    Window.size = (300,300)
+    def initController(self,broker='127.0.0.1', port=1883, topic='controllerTest'):
+        tracker = 0
+        sensorData = ''
+        #broker is me
+        self.cliePub = controlPub.controlPublisher()
+
+        self.cliePub.buildPublisher()
+        self.cliePub.addPublisherParams(broker, port, topic)
+
+        self.cliePub.connectPublisher()
+
+        d = self.cliePub.checkConnection()
+        c = self.cliePub.checkClientDetails()
+
+        print("Publisher details")
+        print(c)
+        print("Publisher is connected: ")
+        print(d)
+        pubClient = self.cliePub.getClient()
+        print(pubClient)
+        print('Here in the publisher')
+        self.cliePub.initController()
+        self.cliePub.actionTypeCreator()
+
+    def Xboxcontroller(self,dt):
+        mvmntData = self.cliePub.actionIdentifier()
+
+        print('Sending mvmnt data to the subscriber')
+        print('\n')
+        print(mvmntData)
+        #time.sleep(1)
+        status = self.cliePub.publishMvmnt(mvmntData)
+        print('Data published')
 
 class MainScreen(Screen):
     def __init__(self, **kwargs):
         super(MainScreen, self).__init__(**kwargs)
         Window.size = (900,600)
-        Gg = TheGuage()
-        g = Gg.gauge(labels=['LOW','MEDIUM','HIGH','VERY HIGH','EXTREME','CRITICAL'],colors='YlOrRd_r', arrow=1, title='Air Quality')
-        self.ids.guage1.add_widget(g)
+
+        self.Gg = TheGuage()
+        self.gFig,self.gAx,self.gArrow = self.Gg.gauge(title='Air Quality', colors='Reds_r')
+        self.canvas3 = self.gFig.canvas
+        self.ids.guage1.add_widget(self.canvas3)
+
+
+        self.index = ["Ambient Temperature"]
+        temp = [11]
+        self.temperature = pd.DataFrame({'Ambient Temperature': temp}, index=self.index)
+        self.fig4, self.ax4 = plt.subplots()
+        self.fig4.patch.set_alpha(0.0)
+
+        self.ax4.tick_params(axis='x', colors='white')
+        self.ax4.tick_params(axis='y', colors='white')
+        self.bar = self.fig4.canvas
+        self.temperature.plot(ax=self.ax4, kind='bar', color='black', legend=False, title='Ambient Temperature', rot=0, ylim=(0,100))
+        self.ids.temperature.add_widget(self.bar)
+        self.celcius = True
+
+        mqttCon,mqttdetails = self.initializeMqtt()
+        if mqttCon == 1:
+            self.connectionStatus = Popup(title='Connection Status', content=Label(text='Data connection successful'),size_hint=(.4,.4),auto_dismiss=(True))
+        elif mqttCon == 0:
+            self.connectionStatus = Popup(title='Connection Status', content=Label(text='Data connection not successful'))
+
+        self.connectionStatus.open()
+
+        Clock.schedule_interval(self.updateData,1)
+
+        Clock.schedule_interval(self.updateTemp,1)
         self.graphit()
+        Clock.schedule_interval(self.updateGuage,1/15)
+
         self.fullscreen = False
         self.freeze = False
+
+    def initializeMqtt(self,broker='143.215.95.16', port=1883, topic='guiParseTest'):
+        self.clieSub = guiSub.guiSubscriber()
+        self.clieSub.buildSubscriber()
+        self.clieSub.addSubscriberParams(broker, port, topic)
+        self.clieSub.connectSubscriber()
+        a = self.clieSub.checkConnection()
+        b = self.clieSub.checkClientDetails()
+        # print("Subscriber details")
+        print(b)
+        # print("Subscriber is connected: ")
+        print(a)
+        subClient = self.clieSub.getClient()
+        print(subClient)
+        print('Here in the subscriber')
+        return a, b
+
+    def updateData(self,dt):
+        print('Data received')
+        sensorData = self.clieSub.receiveGuiData()
+        print('Output from receive Gui Data')
+        print(sensorData)
+        # tracker += 1
+        # print('At ' + str(tracker) + ' second ' + str(status))
+        self.Data = self.clieSub.parseGuiData(sensorData)
+        print('Data parsed into dictionary')
+        print(self.Data)
+        print('\n')
+
+    def updateTemp(self,dt):
+        self.ax4.clear()
+        tempC = self.Data.get('AmbientTempSensor')
+        print(tempC)
+        if self.celcius:
+            self.temp = tempC
+            ymax = 100
+        else:
+            self.temp = 9.0 / 5.0 * tempC + 32
+            ymax = 215
+
+        if self.temp <= 25:
+            color = 'blue'
+        elif self.temp > 25 and self.temp <= 50:
+            color = 'yellow'
+        elif self.temp > 50 and self.temp <= 75:
+            color = 'orange'
+        else:
+            color = 'red'
+        self.temperature = pd.DataFrame({'Ambient Temperature': self.temp}, index=self.index)
+        self.temperature.plot(ax=self.ax4, kind='bar', color=color, legend=False, title='Ambient Temperature', rot=0, ylim=(0,ymax))
+        self.fig4.canvas.draw()
+        self.ids.Templabel.text = str(self.temp)+u'\N{DEGREE SIGN}'+"C"
+
+    def updateGuage(self,dt):
+        self.gArrow.remove()
+        airQuality = self.Data.get('AirQualitySensor')
+        pos = 180 - abs(10-airQuality)*10
+        if pos > 180:
+            pos = 180
+        elif pos < 0:
+            pos = 0
+
+        self.gArrow = self.gAx.arrow(0, 0, 0.225 * np.cos(np.radians(pos)), 0.225 * np.sin(np.radians(pos)), \
+                       width=0.04, head_width=0.09, head_length=0.1, fc='k', ec='k')
+        self.gFig.canvas.draw()
 
     def press(self,event):
         print('press released from test', event.x, event.y, event.button)
@@ -263,6 +285,8 @@ class MainScreen(Screen):
 
     def graphit(self):
         self.fig, (self.ax,self.ax2) = plt.subplots(nrows=2,sharex=True)
+        self.ax2.set_xlabel('common xlabel')
+        self.ax2.set_ylabel('common ylabel')
         self.sensors = Sensors()
 
         self.fig.canvas.mpl_connect('button_press_event', self.press)
@@ -274,58 +298,53 @@ class MainScreen(Screen):
         self.fig.canvas.mpl_connect('close_event', self.close)
 
         canvas = self.fig.canvas
-        self.a = BoxLayout(orientation="vertical")
+        self.liveGraph = BoxLayout(orientation="vertical")
         nav1 = NavigationToolbar2Kivy(canvas)
-        self.a.add_widget(nav1.actionbar)
-        self.a.add_widget(canvas)
-        self.ids.graph.add_widget(self.a)
+        self.liveGraph.add_widget(nav1.actionbar)
+        self.liveGraph.add_widget(canvas)
+        self.ids.graph.add_widget(self.liveGraph)
+        self.ax2.set_ylabel('common ylabel')
 
         self.graphData = pd.DataFrame(columns=('time','audio','temperature'))
-
-        Clock.schedule_interval(self.refreshGraphData,1/800)
+        Clock.schedule_interval(self.refreshGraphData,1)
 
     def refreshGraphData(self,dt):
-        if self.fullscreen == False:
-            #self.ids.graph.remove_widget(self.a)
-            print("Refreshing data...")
-            y = random.randint(1, 50)
-            y2 = random.randint(1,70)
-            newdata = [datetime.datetime.now(), y, y2]
-            self.graphData.loc[-1] = newdata  # adding a row
-            self.graphData.index = self.graphData.index + 1  # shifting index
-            self.graphData = self.graphData.sort_index()  # sorting by index
-            self.graphData.append(newdata, ignore_index=False)
-            self.graphData.reset_index()
-            self.graphData['time'] = pd.to_datetime(self.graphData['time'], unit='s')
-            self.graphData = self.graphData.head(1000)
-
-            #ax.clear()
-            if self.freeze == False:
-                graph = self.graphData.head(300)
-                self.ax.clear()
-                self.ax2.clear()
-                graph.plot(x='time', y='audio', ax=self.ax, kind='line', color='black', legend=False)
-                graph.plot(x='time', y='temperature', ax=self.ax2, kind='line', color='black', legend=False)
-                ## Rotate date labels automatically
-                self.fig.autofmt_xdate()
-                self.fig.canvas.draw()
-                myFmt = DateFormatter('%H:%M:%S')
-                self.ax.xaxis.set_major_formatter(myFmt)
-            else:
-                pass
-            #self.ids.graph.add_widget(self.a)
+        self.ax.set_ylabel('hesfidoj')
+        self.ax2.set_ylabel('common ylabel')
+        y = self.Data.get('ObjectTempSensor')
+        y2 = random.randint(1, 70)
+        newdata = [datetime.datetime.now(), y, y2]
+        self.graphData.loc[-1] = newdata  # adding a row
+        self.graphData.index = self.graphData.index + 1  # shifting index
+        self.graphData = self.graphData.sort_index()  # sorting by index
+        self.graphData.append(newdata, ignore_index=False)
+        self.graphData.reset_index()
+        self.graphData['time'] = pd.to_datetime(self.graphData['time'], unit='s')
+        self.graphData = self.graphData.head(800)
+        if self.fullscreen == False and self.freeze == False:
+            graph = self.graphData.head(300)
+            self.ax.clear()
+            self.ax2.clear()
+            graph.plot(x='time', y='audio', ax=self.ax, kind='line', color='black', legend=False)
+            graph.plot(x='time', y='temperature', ax=self.ax2, kind='line', color='black', legend=False)
+            ## Rotate date labels automatically
+            self.fig.autofmt_xdate()
+            self.fig.canvas.draw()
+            myFmt = DateFormatter('%H:%M:%S')
+            self.ax.xaxis.set_major_formatter(myFmt)
+        else:
+            pass
 
     def changesize(self):
-        print('this')
         if self.fullscreen:
             self.ids.cambox.size_hint = (.5,.5)
             self.ids.fullscreen.text = "Fullscreen"
-            self.ids.graph.add_widget(self.a)
+            self.ids.graph.add_widget(self.liveGraph)
             self.fullscreen = False
         else:
             self.ids.cambox.size_hint = (1,1)
             self.ids.fullscreen.text = "Minimize"
-            self.ids.graph.remove_widget(self.a)
+            self.ids.graph.remove_widget(self.liveGraph)
             self.fullscreen = True
 
     def dostart(self, *largs):
@@ -340,23 +359,25 @@ class Graphs(Screen):
     def __init__(self,**kwargs):
         super(Graphs,self).__init__(**kwargs)
 
-        self.a = BoxLayout(orientation="vertical")
+        self.graph = BoxLayout(orientation="vertical")
         self.fig, (self.ax,self.ax2) = plt.subplots(nrows=2,sharex=True)
-        self.ids.graph.add_widget(self.a)
+        self.ids.graph.add_widget(self.graph)
         canvas = self.fig.canvas
         nav1 = NavigationToolbar2Kivy(canvas)
-        self.a.add_widget(nav1.actionbar)
-        self.a.add_widget(canvas)
+        self.graph.add_widget(nav1.actionbar)
+        self.graph.add_widget(canvas)
         self.fig.canvas.mpl_connect('key_press_event', self.keypress)
+
+    def on_enter(self, *args):
+        self.manager.get_screen('main').onMain=False
 
     def keypress(self,event):
         print('key press', event.key)
         if str(event.key) == 't':
             self.manager.current = 'main'
 
-    def thing(self):
+    def redrawGraph(self):
         graph = self.manager.get_screen('main').graphData.head(1000)
-        print(graph)
         self.ax.clear()
         self.ax2.clear()
         graph.plot(x='time', y='audio', ax=self.ax, kind='line', color='black', legend=False)
@@ -368,11 +389,11 @@ class Graphs(Screen):
 
 
 
-sm = ScreenManager()
-#sm.add_widget(LoginScreen(name='login'))
-sm.add_widget(MainScreen(name='main'))
+sm = ScreenManager(transition=SwapTransition())
+sm.add_widget(LoginScreen(name='login'))
+#sm.add_widget(MainScreen(name='main'))
 #sm.add_widget(Options(name='options'))
-sm.add_widget(Graphs(name='graphs'))
+#sm.add_widget(Graphs(name='graphs'))
 
 
 class MyApp(App):
